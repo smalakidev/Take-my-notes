f96da769d7f714fa4ca2df9587535459cbba59cf
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-03-20 13:21:28 -0400

    made changes to the file routes inthe APIS

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,3 @@
-/ ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
-
 
 // ===============================================================================
 // ROUTING
@@ -16,7 +7,6 @@ module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
   app.get("/api/tables", function(req, res) {
@@ -35,7 +25,7 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/notes", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
@@ -50,10 +40,10 @@ module.exports = function(app) {
   });
 
   // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+ //clear out the table while working with the functionality.
+  
 
-  app.post("/api/clear", function(req, res) {
+  app.post("/api/notes", function(req, res) {
     // Empty out the arrays of data
     tableData.length = 0;
     waitListData.length = 0;
