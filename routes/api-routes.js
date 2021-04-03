a8c2cc4fb282ab49eea59682c52a548d92017aea
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-04-03 12:57:39 -0400

    Working on the delete portion of the note taker app

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -55,42 +55,11 @@ res.json({ok: true})
 
 
   };
+//This will be for the delete portion of the Note Taker
+  app.delete("/api/notes/:id", function(req, res) {
+    let id = req.params.id
 
-  // app.get("/api/notes", function(req, res) {
-  //   res.json(waitListData);
-  // });
+    fs.readFile
+    
+  });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
-  // app.post("/api/notes", function(req, res) {
-  //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-  //   // It will do this by sending out the value "true" have a table
-  //   // req.body is available since we're using the body parsing middleware
-  //   if (tableData.length < 5) {
-  //     notes.push(req.body);
-  //     res.json(true);
-  //   }
-  //   else {
-  //     notes.push(req.body);
-  //     res.json(false);
-  //   }
-  // });
-
-  // ---------------------------------------------------------------------------
- //clears out the table while working with the functionality.
-  
-
-  // app.post("/api/notes", function(req, res) {
-  //   // Empty out the arrays of data
-  //   notes.length = 0;
-  //   notes.length = 0;
-
-//     res.json({ ok: true });
-//   });
-// };
